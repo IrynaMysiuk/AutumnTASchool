bd779b9820224832e2d5f23c120c29b2e30e6e35
Iryna Mysiuk <iruna.musyk8a@gmail.com>
2019-11-13 17:21:10 +0200

    Add test for task 5

--- src/test/java/TrainingByTests.java
+++ src/test/java/TrainingByTests.java
@@ -135,18 +135,52 @@ public class TrainingByTests {
         driver.findElement(By.xpath("//ul[@class=\"main-nav__list\"]/li/a[@class=\"topNavItem news click hover\"]")).click();
         WebElement newsTitle = wait.until(ExpectedConditions
                 .visibilityOfElementLocated(By.xpath("//div[@class=\"container\"]/div[@class=\"section-ui__title ng-binding\"]")));
-
         Assert.assertEquals(newsTitle.getText(), "NEWS", "News title is incorrect");
         List<WebElement> newsArticles = driver.findElements(By.xpath("//div[@ng-repeat=\"category in categories\"]/span[@class=\"ng-binding\"]"));
         newsArticles.forEach(webElement -> Assert.assertTrue(webElement.isDisplayed(), String.format("Article %s is not displayed", webElement.getText())));
         Thread.sleep(10000);
 
-        WebElement materialsItem=driver.findElement(By.xpath("//span[@class=\"ng-binding\" and contains(text(), \"Materials\")]"));
+        WebElement materialsItem = driver.findElement(By.xpath("//span[@class=\"ng-binding\" and contains(text(), \"Materials\")]"));
         Assert.assertEquals(materialsItem.getText(), "MATERIALS", "Materials is not correct");
         driver.findElement(By.xpath("//div[@class=\"news-page-item__title\"]"));
         List<WebElement> materialsOrUsefulItem = driver.findElements(By.xpath(" //div[@class=\"news-page-item__title\"]"));
-        materialsOrUsefulItem.forEach(webElement -> Assert.assertFalse( webElement.getText().contains("useful") ||
-                webElement.getText().contains("materials"),String.format("Link %s is not contained \"useful\" or \"materials\" words", webElement)));
+        materialsOrUsefulItem.forEach(webElement -> Assert.assertFalse(webElement.getText().contains("useful") ||
+                webElement.getText().contains("materials"), String.format("Link %s is not contained \"useful\" or \"materials\" words", webElement)));
+    }
+
+    @Test(description = "Verify ‘Trainings’ search works properly with searching in ‘Locations’.")
+    public void verifyTrainingsSearchWorksProperlyWithSearchingInLocations() throws InterruptedException {
+        changeLanguage("English");
+
+        WebElement signInButton = driver.findElement(By.xpath("//p[@class='header-auth__signin']//span"));
+        signInButton.click();
+
+        WebElement signInModalButton = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector(".popup-reg-sign-in-form__sign-in")));
+        Assert.assertEquals(signInModalButton.getAttribute("value"), "Sign in", "'Sign in' in modal window is not displayed");
+        WebElement mailInput = driver.findElement(By.id("signInEmail"));
+        mailInput.sendKeys(CORRECT_LOGIN);
+        WebElement passwordInput = driver.findElement(By.id("signInPassword"));
+        passwordInput.sendKeys(CORRECT_PASSWORD);
+        signInModalButton.click();
+
+        WebElement userName = driver.findElement(By.xpath("//div[@class=\"user-info__name\"]/parent::a"));
+        Assert.assertEquals(userName.getText(), "Iryna Mysiuk", "User Name is not correct");
+        Thread.sleep(10000);
+        driver.findElement(By.xpath("//ul[@class=\"main-nav__list\"]/li/a[contains(text(),\"Training list\")]")).click();
+        WebElement trainingListTitle = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.xpath("//div[@class=\"section-ui__title ng-binding\" and contains(text(),\"OUR SKILLS\")]")));
+        Assert.assertEquals(trainingListTitle.getText(), "OUR SKILLS", "Training list is incorrect");
+
+        driver.findElement(By.xpath("//div[@ng-click=\"openChoose()\"]")).click();
+        List<WebElement> searchForTraining = driver.findElements(By.xpath("//div[@ng-click=\"changeTab('Location')\"]"));
+        searchForTraining.forEach(webElement -> Assert.assertTrue(webElement.isDisplayed(),
+                String.format("Search for training with countries %s is not displayed", webElement.getText())));
+        Thread.sleep(10000);
+
+        driver.findElement(By.xpath("//div[@ng-click=\"activeCountryChoose(locations)\" and contains(text(),\"Ukraine\")]")).click();
+        driver.findElement(By.xpath("//label[@class=\"location__not-active-label ng-binding\" and contains(.,'Lviv')]")).click();
+
     }
 
     public void changeLanguage(String language) {
