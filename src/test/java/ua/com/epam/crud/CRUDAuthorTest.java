b7919f82d4af64d578203ce99d4a9ec1e77fc37a
Roman <roman.shmandrovskyj.ki.2014@gmail.com>
2019-11-22 17:48:56 +0200

    - init commit

--- src/test/java/ua/com/epam/crud/CRUDAuthorTest.java
+++
@@ -1,9 +0,0 @@
-package ua.com.epam.crud;
-
-import org.testng.annotations.Test;
-import ua.com.epam.BaseTest;
-
-@Test(description = "CRUD operations for author table")
-public class CRUDAuthorTest extends BaseTest {
-
-}
