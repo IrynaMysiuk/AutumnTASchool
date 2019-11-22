21ba59c91e6d2e141da7c54ea0dd5bb0895d2583
Roman <roman.shmandrovskyj.ki.2014@gmail.com>
2019-11-22 19:41:21 +0200

    - add tests

---
+++ src/test/java/ua/com/epam/CRUDAuthorTest.java
@@ -0,0 +1,59 @@
+package ua.com.epam;
+
+import org.json.JSONArray;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ua.com.epam.entity.author.Author;
+
+import java.util.List;
+
+import static ua.com.epam.config.URI.GET_ALL_AUTHORS_ARR;
+import static ua.com.epam.config.URI.POST_AUTHOR_SINGLE_OBJ;
+
+@Test(description = "")
+public class CRUDAuthorTest extends BaseTest {
+    private Author expAuthor = testData.authors().getRandomOne();
+    private List<Author> authorList = testData.authors().getDefaultAuthors();
+
+    @BeforeMethod
+    public void sendAuthors() {
+        for(Author a : authorList) {
+            client.post(POST_AUTHOR_SINGLE_OBJ, a);
+        }
+    }
+
+    @Test(description = "post single Author obj")
+    public void postAuthor() {
+        client.post(POST_AUTHOR_SINGLE_OBJ, expAuthor);
+
+        int statusCode = client.getResponse().getStatusCode();
+        String body = client.getResponse().getBody();
+
+        Author actAuthor = g.fromJson(body, Author.class);
+
+        Assert.assertEquals(statusCode, 201);
+        Assert.assertEquals(expAuthor, actAuthor);
+    }
+
+    @Test
+    public void getDifferentAuthors() {
+        String params = "?orderType=asc&page=1&pagination=true&size=5&sortBy=authorId";
+
+        client.get(GET_ALL_AUTHORS_ARR + params);
+        int statusCode = client.getResponse().getStatusCode();
+        String body = client.getResponse().getBody();
+
+        JSONArray authorsArr = new JSONArray(body);
+        int len = authorsArr.length();
+
+        Assert.assertEquals(statusCode, 200);
+        Assert.assertEquals(len, 5);
+    }
+
+    @AfterMethod
+    public void cleanUp() {
+        clean.authors();
+    }
+}
